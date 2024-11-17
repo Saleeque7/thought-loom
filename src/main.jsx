1af74b3d783646fe528bc06b906919ad92797ea0
saleeque <saleeque00cr7@gmail.com>
2024-11-17 18:40:37 +0530

    first commit for production edit vercel.json

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,7 @@ createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
       <PersistGate persistor={persistor}>
-        <BrowserRouter basename="/thought-loom">
+        <BrowserRouter >
           <ToastContainer />
           <App />
         </BrowserRouter>
